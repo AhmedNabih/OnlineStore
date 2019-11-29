225d5e5c67d652538d43cca32f0afd7a4c4feb9d
Ahmed Nabih <ahmed.nabih01@gmail.com>
2019-11-29 15:10:36 +0200

    bugs fixed

--- Users/Admins/AdminStatisticsSystem/Receivers/ReceiverStrategyPattern/IAverage.cs
+++ Users/Admins/AdminStatisticsSystem/Receivers/ReceiverStrategyPattern/IAverage.cs
@@ -1,5 +1,4 @@
-﻿using OnlineStore.Users.Admins.AdminStatisticsSystem.Receivers.ReceiverStrategyPattern;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OnlineStore.Users.Admins.AdminsStatisticsSystemCommands.Receivers.ReceiverStrategyPattern
 {
-    public interface IAverage : IReceiverCommands
+    public interface IAverage
     {
         public abstract String Average();
     }
--- Users/Admins/AdminStatisticsSystem/Receivers/ReceiverStrategyPattern/IMin.cs
+++ Users/Admins/AdminStatisticsSystem/Receivers/ReceiverStrategyPattern/IMin.cs
@@ -1,5 +1,4 @@
-﻿using OnlineStore.Users.Admins.AdminStatisticsSystem.Receivers.ReceiverStrategyPattern;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OnlineStore.Users.Admins.AdminsStatisticsSystemCommands.Receivers.ReceiverStrategyPattern
 {
-    public interface IMin : IReceiverCommands
+    public interface IMin
     {
         public abstract String Min();
     }
